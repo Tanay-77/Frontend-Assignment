a54e1e282faeea8bf1698801bc8e1df39966eb74
Tanay Mahajan <tanaymahajan7@gmail.com>
2026-08-18 18:13:58 +0530

    fixed nav again

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const isActive = (path: string) => location.pathname === path;
 
   return (
-    <nav className="fixed w-full z-50 py-4 px-4 md:px-8">
+    <nav className="absolute w-full z-50 py-4 px-4 md:px-8">
       <div className="max-w-7xl mx-auto flex justify-between items-center relative h-12">
         
         {/* Logo */}
